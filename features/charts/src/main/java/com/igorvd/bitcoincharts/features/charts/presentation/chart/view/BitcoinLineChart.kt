9026ec5e2a8173e810991f5d910c717fbc7d4f39
Igor Vilela <igorvd@ciandt.com>
2021-05-19 20:55:05 -0300

    Add extra top offset to avoid label clipping

--- features/charts/src/main/java/com/igorvd/bitcoincharts/features/charts/presentation/chart/view/BitcoinLineChart.kt
+++ features/charts/src/main/java/com/igorvd/bitcoincharts/features/charts/presentation/chart/view/BitcoinLineChart.kt
@@ -49,7 +49,8 @@ class BitcoinLineChart @JvmOverloads constructor(
         isHighlightPerTapEnabled = true
         isHighlightPerDragEnabled = true
         minOffset = 0f
-        extraBottomOffset = CHART_EXTRA_BOTTOM_OFFSET
+        extraTopOffset = CHART_EXTRA_OFFSET
+        extraBottomOffset = CHART_EXTRA_OFFSET
         marker = CustomMarkerView(context)
     }
 
@@ -126,7 +127,7 @@ class BitcoinLineChart @JvmOverloads constructor(
     }
 
     companion object {
-        private const val CHART_EXTRA_BOTTOM_OFFSET = 16f
+        private const val CHART_EXTRA_OFFSET = 16f
         private const val CHART_MAX_HIGHLIGHT_DISTANCE = 24f
         private const val X_AXIS_Y_OFFSET = 16f
         private const val X_AXIS_LABEL_COUNT = 4

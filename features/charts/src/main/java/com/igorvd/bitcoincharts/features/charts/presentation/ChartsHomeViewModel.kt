eea62efaf520ad0fdedca445360e8de60a0c3aa0
Igor Vilela <igorvd@ciandt.com>
2021-05-18 22:58:55 -0300

    Remove unused import

--- features/charts/src/main/java/com/igorvd/bitcoincharts/features/charts/presentation/ChartsHomeViewModel.kt
+++ features/charts/src/main/java/com/igorvd/bitcoincharts/features/charts/presentation/ChartsHomeViewModel.kt
@@ -2,7 +2,6 @@ package com.igorvd.bitcoincharts.features.charts.presentation
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.igorvd.bitcoincharts.features.charts.data.network.api.BlockchainApi
 import com.igorvd.bitcoincharts.features.charts.domain.model.ChartType
 import com.igorvd.bitcoincharts.features.charts.domain.usecase.GetChartUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel

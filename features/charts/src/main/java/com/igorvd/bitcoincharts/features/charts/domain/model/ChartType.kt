8d09c2eb97f7747adbfb25948a7e02472830e9f1
Igor Vilela <igorvd@ciandt.com>
2021-05-20 12:26:26 -0300

    Minor improvements

--- features/charts/src/main/java/com/igorvd/bitcoincharts/features/charts/domain/model/ChartType.kt
+++ features/charts/src/main/java/com/igorvd/bitcoincharts/features/charts/domain/model/ChartType.kt
@@ -2,6 +2,6 @@ package com.igorvd.bitcoincharts.features.charts.domain.model
 
 enum class ChartType(val query: String) {
 
-    MARKET_PRICE("market-price")
-
+    MARKET_PRICE("market-price"),
+    TRANSACTION_FEES("transaction-fees")
 }
--- features/charts/src/main/java/com/igorvd/bitcoincharts/features/charts/presentation/home/ChartsHomeActivity.kt
+++ features/charts/src/main/java/com/igorvd/bitcoincharts/features/charts/presentation/home/ChartsHomeActivity.kt
@@ -2,25 +2,25 @@ package com.igorvd.bitcoincharts.features.charts.presentation.home
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.viewModels
-import com.igorvd.bitcoincharts.core.presentation.extensions.launch
-import com.igorvd.bitcoincharts.features.charts.R
+import androidx.appcompat.app.AppCompatActivity
+import com.igorvd.bitcoincharts.core.presentation.extensions.viewBinding
+import com.igorvd.bitcoincharts.features.charts.databinding.ActivityChartsHomeBinding
 import com.igorvd.bitcoincharts.features.charts.presentation.chart.BitcoinChartActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ChartsHomeActivity : AppCompatActivity() {
 
-    private val chartsHomeViewModel: ChartsHomeViewModel by viewModels()
+    private val viewModel: ChartsHomeViewModel by viewModels()
+    private val viewBinding: ActivityChartsHomeBinding by viewBinding(ActivityChartsHomeBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_charts_home)
+        setContentView(viewBinding.root)
 
-        findViewById<Button>(R.id.button).setOnClickListener {
+        viewBinding.button.setOnClickListener {
             val intent = BitcoinChartActivity.newIntent(this)
             startActivity(intent)
         }
